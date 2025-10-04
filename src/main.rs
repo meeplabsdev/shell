a86c6af857944011a3c10c97ecd54a2b4ca8a2c6
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-10-04 20:50:46 +0100

    windows support

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,35 @@ mod tests;
 
 use crate::shell::Shell;
 use error::Error;
+use std::{io, thread};
+
+#[cfg(not(windows))]
 use signal_hook::{
     consts::{SIGINT, SIGQUIT, SIGTSTP},
     iterator::Signals,
 };
-use std::{io, thread};
 
 fn main() -> Result<(), Error> {
-    let mut signals = Signals::new([SIGINT, SIGQUIT, SIGTSTP]).unwrap();
     let mut shell = Shell::new(io::stdin(), io::stdout(), io::stderr());
 
-    thread::spawn(move || {
-        let mut shell = Shell::new(io::stdin(), io::stdout(), io::stderr());
-        for sig in signals.forever() {
-            let common_name = match sig {
-                SIGINT => "CANCEL",
-                SIGQUIT => "DISCONNECT",
-                SIGTSTP => "FORCE STOP",
-                _ => "unknown",
-            };
+    #[cfg(not(windows))]
+    {
+        let mut signals = Signals::new([SIGINT, SIGQUIT, SIGTSTP]).unwrap();
 
-            shell.writeln(format!("signal {}", common_name)).ok();
-        }
-    });
+        thread::spawn(move || {
+            let mut shell = Shell::new(io::stdin(), io::stdout(), io::stderr());
+            for sig in signals.forever() {
+                let common_name = match sig {
+                    SIGINT => "CANCEL",
+                    SIGQUIT => "DISCONNECT",
+                    SIGTSTP => "FORCE STOP",
+                    _ => "unknown",
+                };
+
+                shell.writeln(format!("signal {}", common_name)).ok();
+            }
+        });
+    }
 
     let mut exit_code = 0;
     for _ in 0..5 {
